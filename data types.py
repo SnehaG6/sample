427fda63363a8ce89ce63b2893e497885e0ad3c4
SnehaG6 <168833375+SnehaG6@users.noreply.github.com>
2024-05-06 13:05:25 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,10 @@
+x = "Hello World"
+x = 50
+x = 60.5
+x = 3j
+x = ["geeks", "for", "geeks"]
+x = ("geeks", "for", "geeks")
+x = {"name": "Suraj", "age": 24}
+x = {"geeks", "for", "geeks"}
+x = True # bool 
+x = b"Geeks" # binary
