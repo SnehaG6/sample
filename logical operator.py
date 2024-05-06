7ae7f8ecbbe0e7a6f1bdf8243454b1738e8bd79a
SnehaG6 <168833375+SnehaG6@users.noreply.github.com>
2024-05-06 17:20:09 +0530

    Add files via upload

---
+++ logical operator.py
@@ -0,0 +1,13 @@
+
+a = 12
+b = 26
+c = 4
+
+if a > b and a > c: 
+	print("Number a is larger")
+
+if b > a and b > c:
+	print("Number b is larger")
+
+if c > a and c > b:
+	print("Number c is larger")
