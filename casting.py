3fd651b0307330d93b43dc946cb0e14755178316
SnehaG6 <168833375+SnehaG6@users.noreply.github.com>
2024-05-07 15:22:34 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,6 @@
+num1 = 6
+num2 = 5.7
+#Mathematical operations between float and int automatically casts the resulting value into a float
+num3 = num1 + num2
+print(num3)  #prints the value 11.7
+print(type(num3)) # prints <float>
